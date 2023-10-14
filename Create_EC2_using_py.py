b8d8877654d71beeb591ccabfdb7f7428cac5adf
L00157158 <L00157158@atu.ie>
2023-10-14 09:32:39 +0100

    Configuration file to create EC2 instance

--- Create_EC2_using_py.py
+++ Create_EC2_using_py.py
@@ -1,21 +1,45 @@
 import boto3
+import yaml
+import os
+
+def read_config_from_yaml(file_path):
+    
+    print(f"File path trying to reach: {file_path}")
+    
+    try:
+        with open(file_path, 'r') as file:
+            config = yaml.safe_load(file)
+            print(f"Config Loaded: {config}")  # Debugging print
+            return config
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+    except yaml.YAMLError as e:
+        print(f"Error in YAML file: {e}")
+        return None
 
 def ec2_demo():
-    ec2 = boto3.resource('ec2')
+  
+    # Read the entire config from the YAML file
+    config = read_config_from_yaml(r'c:\Users\pc\PGDIP\IaC\EC2-config.yaml')
 
+    # Check if the config is loaded correctly
+    if config is None:
+        print("Configuration is empty, exiting.")
+        return
+
+    ec2 = boto3.resource('ec2')
+    
     demo_instance = ec2.create_instances(
-        ImageId='ami-0bb4c991fa89d4b9b',
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName='l00157158_key_pair',
+        ImageId=config['ImageId'],
+        MinCount=config['MinCount'],
+        MaxCount=config['MaxCount'],
+        InstanceType=config['InstanceType'],
+        KeyName=config['KeyName'],
         TagSpecifications=[
             {
                 'ResourceType': 'instance',
-                'Tags': [
-                    {'Key': 'Name', 'Value': 'L00157158-EC2-02'},
-                    {'Key': 'Environment', 'Value': 'PGDIP-Dev'}
-                ]
+                'Tags': config['Tags']
             }
         ]
     )
@@ -23,4 +47,4 @@ def ec2_demo():
 
 if __name__ == '__main__':
     ec2_demo()
-    print("Instance successfully created")
+    print("Created?")
